d8e65013dfdd647ca5b368ad4cb8473aff1042e7
MSI <anonymous.zebro@gmail.com>
2022-09-20 18:52:47 +0200

    removed insurance policies list from EmployeeResponse class

--- src/main/java/pl/azebrow/harvest/response/EmployeeResponse.java
+++ src/main/java/pl/azebrow/harvest/response/EmployeeResponse.java
@@ -2,12 +2,9 @@ package pl.azebrow.harvest.response;
 
 import lombok.Data;
 
-import java.util.Collection;
-
 @Data
 public class EmployeeResponse {
     private AccountResponse account;
-    private Collection<InsuranceResponse> policies;
     private String code;
     private String phoneNumber;
     private Boolean passportTaken;
