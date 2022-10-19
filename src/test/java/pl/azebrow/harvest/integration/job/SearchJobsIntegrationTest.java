b8c96b91e31fe7ba6e3d3aed09176d7b3b4f5cc8
MSI <anonymous.zebro@gmail.com>
2022-10-19 18:15:29 +0200

    first int tests

--- src/test/java/pl/azebrow/harvest/integration/job/SearchJobsIntegrationTest.java
+++ src/test/java/pl/azebrow/harvest/integration/job/SearchJobsIntegrationTest.java
@@ -1,15 +1,23 @@
 package pl.azebrow.harvest.integration.job;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import pl.azebrow.harvest.integration.BaseIntegrationTest;
 import pl.azebrow.harvest.repository.JobRepository;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class SearchJobsIntegrationTest extends BaseIntegrationTest {
 
     private final static String JOB_URL = "/api/v1/job";
 
     @Autowired
     private JobRepository jobRepository;
+
+    @BeforeAll
+    public void setup() {
+
+    }
 
 
 }
