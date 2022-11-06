faf6abc6f2d666d023e0ebd1cadfb3557b59e03e
MSI <anonymous.zebro@gmail.com>
2022-11-06 21:35:43 +0100

    small login/logout fixes for frontend

--- src/main/java/pl/azebrow/harvest/config/WebSecurityConfig.java
+++ src/main/java/pl/azebrow/harvest/config/WebSecurityConfig.java
@@ -2,31 +2,46 @@ package pl.azebrow.harvest.config;
 
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.FormLoginConfigurer;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.config.annotation.web.configurers.RememberMeConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+
+import javax.servlet.http.HttpServletResponse;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(securedEnabled = true, proxyTargetClass = true)
 @RequiredArgsConstructor
 @EnableJpaAuditing
+@Slf4j
 public class WebSecurityConfig {
 
+    @SuppressWarnings("unused")
     private final UserDetailsService userDetailsService;
 
+    @Value("${harvest.remember-me.key:}")
+    private String rememberMeKey;
+
     @Bean
-    public PasswordEncoder passwordEncoder(){
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
@@ -46,8 +61,32 @@ public class WebSecurityConfig {
                 .antMatchers("/api/v1/recovery/**").anonymous()
                 .antMatchers("/login").permitAll()
                 .anyRequest().authenticated().and()
-                .formLogin();
+                .rememberMe(this::configureRememberMe)
+                .formLogin(this::configureLogin)
+                .logout(this::configureLogout)
+                .exceptionHandling().authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED));
 
         return http.build();
+    }
+
+    private void configureLogout(LogoutConfigurer<HttpSecurity> configurer) {
+        configurer
+                .logoutSuccessHandler(new HttpStatusReturningLogoutSuccessHandler());
+    }
+
+    private void configureLogin(FormLoginConfigurer<HttpSecurity> configurer) {
+        configurer
+                .loginPage("/login").permitAll()
+                .successHandler((_request, _response, _auth) -> System.out.println("do nuffin, success"))
+                .failureHandler((_request, _response, _auth) -> _response.setStatus(HttpServletResponse.SC_UNAUTHORIZED));
+    }
+
+    private void configureRememberMe(RememberMeConfigurer<HttpSecurity> configurer) {
+        if (rememberMeKey.isEmpty()) {
+            log.warn("Using generated remember-me key");
+            return;
+        }
+        configurer.key(rememberMeKey);
+        log.info("Using predefined remember-me key");
     }
 }
